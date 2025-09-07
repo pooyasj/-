d01b6f2cb57f24a76eec10034d626ea8a1290c85
pooya jb <pooyajebreili@gmail.com>
2025-09-07 18:13:15 +0330

    tamrin axios  va map va state

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
-  return <></>;
+  const [name, addname] = useState([]);
+  useEffect(function () {
+    const p = axios.get("https://fakestoreapi.com/carts").then(function (a) {
+       addname(a.data);
+    });
+  }, []);
+  console.log(name)
+  return (
+    <>
+      <ul>
+        {name.map(function (person) {
+        return  <li key={person.id}>{person.userId}</li>;
+        })}
+      </ul>
+    </>
+  );
 }
 
 export default App;
